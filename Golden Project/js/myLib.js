54350fe8c15c76489b5b84ce3cf18ca35a1cb1fb
TycoonTart <72146197+TycoonTart@users.noreply.github.com>
2021-02-09 09:37:19 +0530

    Add files via upload

---
+++ Golden Project/js/myLib.js
@@ -0,0 +1,32 @@
+function createTiger(x,y,width,height){
+    tiger=createSprite(x,y,width,height)
+                tiger.addImage(tigerImg)
+                tiger.visible=false
+                tiger.scale=0.4 
+}
+function createPolarBear(x,y,width,height){
+            polarBear=createSprite(x,y,width,height)
+            polarBear.addImage(polarBearImg)
+            polarBear.scale=0.1
+            polarBear.visible=false
+
+}
+function createFlag(x,y,width,height){
+    flag=createSprite(x,y,width,height)
+    flag.addImage(flagImg)
+    flag.scale=0.5
+    
+
+}
+function createEnd(x,y,width,height){
+    end=createSprite(x,y,width,height)
+    end.addImage(endImg)
+    end.scale=2
+
+}
+function createGameEnd(x,y,width,height){
+    gameEnd=createSprite(500,500,displayWidth,displayHeight)
+    gameEnd.shapeColour("black")
+}
+
+    
